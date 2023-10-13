c31cd0502aeddb02ea8b2eae221b551d6702dce9
andrew mayer <dddron@gmail.com>
2023-10-13 15:14:33 +0400

    added two subviews

--- tips calc/tips calc/MainViewController.swift
+++ tips calc/tips calc/MainViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var actionButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+                
+        titleLabel.text = "hello!"
+        
+        actionButton.setTitle("action", for: .normal)
     }
 
-
+    @IBAction 
+    func doAction(_ sender: Any) {
+        titleLabel.text = "button was pressed"
+    }
+    
 }
 
