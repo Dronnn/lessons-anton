a3187cd59367bffc4e5d86bbd761115d522cd74f
droptopelisey <droptopelisey@gmail.com>
2023-10-16 15:24:21 +0300

    привязал необходимые элементы к коду, но еще не сделан разделитель

--- clone_tips_calc_app/clone_tips_calc_app/MainViewController.swift
+++ clone_tips_calc_app/clone_tips_calc_app/MainViewController.swift
@@ -9,10 +9,37 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+// MARK: - Outlets
+    
+    @IBOutlet weak var amountTextField: UITextField!
+    
+    @IBOutlet weak var percentLabel: UILabel!
+    
+    @IBOutlet weak var splitLabel: UILabel!
+    
+    @IBOutlet weak var tipPerPersonLabel: UILabel!
+    
+    @IBOutlet weak var totalPerPersonLabel: UILabel!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+// MARK: - ACTIONS
+    
+    @IBAction func stepperAction(_ sender: Any) {
+    }
+    
+
+
+// MARK: - EXTENSIONS
+    
+   
     
 }
 
