399120e85753244073436beb089e69f8426eb7ba
andrew mayer <dddron@gmail.com>
2023-10-18 15:25:57 +0400

    progress

--- tips calc/tips calc/MainViewController.swift
+++ tips calc/tips calc/MainViewController.swift
@@ -19,50 +19,102 @@ class MainViewController: UIViewController {
     @IBOutlet weak var totalTipsLabel: UILabel!
     @IBOutlet weak var totalMoneyLabel: UILabel!
     
+    @IBOutlet weak var amountView: UIView!
+    @IBOutlet weak var tipsView: UIView!
+    @IBOutlet weak var totalView: UIView!
+    
     // MARK: - life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        amountTextFieldBecomeFirstRespoder()
+        setupAppearance()
     }
     
     // MARK: - setup
+    
+    private func totalBlock(isHidden: Bool) {
+        totalView.isHidden = isHidden
+    }
+    
+    private func setupAppearance() {
+        amountView.layer.cornerRadius = 10.0
+        tipsView.layer.cornerRadius = 10.0
+        totalView.layer.cornerRadius = 10.0
+    }
     
     
     // MARK: - actions
     
     @IBAction func peopeNumberChanged(_ sender: UIStepper) {
-        //
+        peopleNumberLabel.text = "\(Int(sender.value))"
+        calculate()
+        totalBlock(isHidden: Int(sender.value) < 2)
     }
+    
+    @IBAction func screenTap(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func doAmountTextFieldFirstRespoder(_ sender: UIButton) {
+        amountTextFieldBecomeFirstRespoder()
+    }
+    
+    private func amountTextFieldBecomeFirstRespoder() {
+        amountTextField.becomeFirstResponder()
+    }
+    
+    @IBAction func doRound(_ sender: UIButton) {
+        tipPerPersonLabel.text = ""
+        totoalPerPersonLabel.text = ""
+        totalTipsLabel.text = ""
+        totalMoneyLabel.text = ""
+    }
+
+    @IBAction func showPercentagesViewController(_ sender: UIButton) {
+        
+//      let vc = PercentagesViewController()
+        
+        let vc = UIStoryboard(
+            name: "Main",
+            bundle: nil
+        ).instantiateViewController(
+            withIdentifier: "PercentagesViewController"
+        )
+        
+        present(vc, animated: true)
+    }
+    
 }
 
 // MARK: - logic
 
 extension MainViewController {
     
-    func calculate(with text: String) {
-        guard let money = Int(text) else { return }
-
-        print("money = \(money)")
-        
+    func calculate(with text: String? = nil) {
+        let text = text ?? amountTextField.text ?? "0.0"
         guard
-            let percenText = percentLabel.text,
-            let percent = Int(percenText)
+            let money = Double(text),
+            let percentText = percentLabel.text?
+                .replacingOccurrences(of: "%", with: ""),
+            let percent = Double(percentText),
+            let peopleText = peopleNumberLabel.text,
+            let peopleCount = Double(peopleText)
         else {
             return
         }
         
-        print("percent = \(percent)")
+        let totalTips = money * (percent / 100.0)
+        let totalMoney = money + totalTips
+
+        let tipPerPerson = totalTips / peopleCount
+        let totalPerPerson = totalMoney / peopleCount
         
-        let totalTips = Double(money) * (Double(percent) / 100.0)
-        let totalMoney = Double(money) + totalTips
-        
-        tipPerPersonLabel.text = "qqq"
-        totoalPerPersonLabel.text = "qqq"
-        totalTipsLabel.text = "\(totalTips)"
-        totalMoneyLabel.text = "\(totalMoney)"
+        tipPerPersonLabel.text = String(format: "%.2f", tipPerPerson)
+        totoalPerPersonLabel.text = String(format: "%.2f", totalPerPerson)
+        totalTipsLabel.text = String(format: "%.2f", totalTips)
+        totalMoneyLabel.text = String(format: "%.2f", totalMoney)
     }
-    
 }
 
 // MARK: - UITextFieldDelegate
@@ -73,8 +125,7 @@ extension MainViewController: UITextFieldDelegate {
         shouldChangeCharactersIn range: NSRange,
         replacementString string: String
     ) -> Bool {
-        
-        if 
+        if
             let text = textField.text,
             let textRange = Range(range, in: text) 
         {
---
+++ tips calc/tips calc/PercentagesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PercentagesViewController.swift
+//  tips calc
+//
+//  Created by andrew mayer on 18.10.23.
+//
+
+import UIKit
+
+class PercentagesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+}
