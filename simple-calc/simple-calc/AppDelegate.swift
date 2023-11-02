3a12609682790bffebeb52fd0a5b3bee91f69f19
andrew mayer <dddron@gmail.com>
2023-11-02 14:23:32 +0300

    the simple calc app started

---
+++ simple-calc/simple-calc/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  simple-calc
+//
+//  Created by andrew mayer on 02.11.23.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
