4fb770435d07e0de24e92e548eaa0867ae0d4c27
andrew mayer <dddron@gmail.com>
2023-11-30 11:34:26 +0400

    progress

---
+++ mvvm-demo1/mvvm-demo1/History/HistoryCell.swift
@@ -0,0 +1,32 @@
+//
+//  HistoryCell.swift
+//  mvvm-demo1
+//
+//  Created by andrew mayer on 30.11.23.
+//
+
+import UIKit
+import SnapKit
+
+class HistoryCell: UITableViewCell {
+
+        // MARK: - life cycle
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupCellView()
+    }
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCellView()
+    }
+
+    // MARK: - setups
+
+    private func setupCellView() {
+        backgroundColor = .red
+    }
+
+}
+
