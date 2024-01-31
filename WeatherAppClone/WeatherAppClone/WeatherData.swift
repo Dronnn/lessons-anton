fd992efa62b0dcb882fe4412503eae37a7465fbb
droptopelisey <droptopelisey@gmail.com>
2024-01-31 14:20:30 +0700

    progress

--- WeatherAppClone/WeatherAppClone/WeatherData.swift
+++ WeatherAppClone/WeatherAppClone/WeatherData.swift
@@ -8,12 +8,35 @@
 import Foundation
 
 struct WeatherData: Decodable {
-    let lonCoord: Double
-    let latCoord: Double
-    let weatherID: Int
-    let weatherMain: String
-    let weatherDescription: String
-    let weatherIcon: String
+    let coord: Coordinats
+    let weather: [Weather]
+    let base: String
+    let main: Main
+    let visibility: Int
+    let wind: Wind
+//    let rain: Rain
+    let clouds: Clouds
+    let dt: Int
+//    let sys: System
+    let timezone: Int
+    let id: Int
+    let name: String
+    let cod: Int
+}
+
+struct Coordinats: Codable {
+    let lon: Double
+    let lat: Double
+}
+
+struct Weather: Codable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+struct Main: Codable {
     let temp: Double
     let feelsLike: Double
     let tempMin: Double
@@ -22,20 +45,40 @@ struct WeatherData: Decodable {
     let humidity: Int
     let seaLevel: Int
     let grndLevel: Int
-    let visibility: Int
-    let windSpeed: Double
-    let windDeg: Int
-    let windGust: Double
-    let rainLh: Double
-    let cloudsAll: Int
-    let dt: Int
-    let sysType: Int
-    let sysID: String
+    
+    enum CodingKeys: String, CodingKey {
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case seaLevel = "sea_level"
+        case grndLevel = "grnd_level"
+        case temp, humidity, pressure
+    }
+}
+
+struct Wind: Codable {
+    let speed: Double
+    let deg: Int
+    let gust: Double
+}
+
+struct Rain: Codable {
+    let oneHour: Double
+    enum CodingKeys: String, CodingKey {
+        case oneHour = "1h"
+    }
+}
+
+struct Clouds: Codable {
+    let all: Int
+}
+
+struct System: Codable {
+    let type: Int
+    let id: String
     let country: String
     let sunrise: Int
     let sunset: Int
-    let timezone: Int
-    let cityID: Int
-    let cityName: String
-    let code: Int
 }
+
+
