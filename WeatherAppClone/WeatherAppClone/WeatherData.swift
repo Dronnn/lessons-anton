e1240b5a14d411435defd31f76a41f55bd99195a
droptopelisey <droptopelisey@gmail.com>
2024-01-31 13:30:49 +0700

    progress

--- WeatherAppClone/WeatherAppClone/WeatherData.swift
+++ WeatherAppClone/WeatherAppClone/WeatherData.swift
@@ -8,6 +8,34 @@
 import Foundation
 
 struct WeatherData: Decodable {
-    
-    
+    let lonCoord: Double
+    let latCoord: Double
+    let weatherID: Int
+    let weatherMain: String
+    let weatherDescription: String
+    let weatherIcon: String
+    let temp: Double
+    let feelsLike: Double
+    let tempMin: Double
+    let tempMax: Double
+    let pressure: Int
+    let humidity: Int
+    let seaLevel: Int
+    let grndLevel: Int
+    let visibility: Int
+    let windSpeed: Double
+    let windDeg: Int
+    let windGust: Double
+    let rainLh: Double
+    let cloudsAll: Int
+    let dt: Int
+    let sysType: Int
+    let sysID: String
+    let country: String
+    let sunrise: Int
+    let sunset: Int
+    let timezone: Int
+    let cityID: Int
+    let cityName: String
+    let code: Int
 }
